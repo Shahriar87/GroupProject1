945304a96723308cb8965b61cf4696dd5c41c309
Shahriar87 <shahriar.firoz@gmail.com>
2018-10-06 14:11:01 -0500

    Added Comment in the index

--- ProjectAssets/javascript/project.js
+++ ProjectAssets/javascript/project.js
@@ -1,3 +1,61 @@
+ // Firebase setup & Comment Section: 
+ 
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCn2pxZTnTMzIA27cfSAaaAH9vjfSTyrGE",
+    authDomain: "stock-memers.firebaseapp.com",
+    databaseURL: "https://stock-memers.firebaseio.com",
+    projectId: "stock-memers",
+    storageBucket: "stock-memers.appspot.com",
+    messagingSenderId: "781450269578"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", postComment);
+
+const timeStamp = () => {
+    let options = {
+        month: '2-digit',
+        day: '2-digit',
+        year: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    };
+    let now = new Date().toLocaleString('en-US', options);
+    return now;
+};
+
+function postComment(e) {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let comment = document.getElementById("comment").value;
+
+    if (name && comment) {
+        database.ref().push({
+            name: name,
+            comment: comment,
+            time: timeStamp()
+        });
+    }
+
+    document.getElementById("name").value = '';
+    document.getElementById("comment").value = '';
+};
+
+database.ref().on("child_added", function (snapshot) {
+    let comment = snapshot.val();
+    addComment(comment.name, comment.comment, comment.time);
+});
+
+const addComment = (name, comment, timeStamp) => {
+    let comments = document.getElementById("comments");
+    comments.innerHTML = `<hr><h4>${name} says<span id="timeSpan">${timeStamp}</span></h4><p>${comment}</p>${comments.innerHTML}`;
+}
+
 // basic javascript filler\
 
 var apiUrl = "https://ronreiter-meme-generator.p.mashape.com/meme";
@@ -7,7 +65,6 @@ var font = "Impact";
 var fontSize = "50";
 var meme = "Metal-Jesus";
 var queryURL = apiUrl+"?bottom="+bottomText+"&font="+font+"&font_size="+fontSize+ "&meme="+meme +"&top="+topText;
-
 
 $.ajax({
     url: queryURL,
@@ -24,10 +81,6 @@ $.ajax({
 
 var goodImg = ["Corona","Buddy-Christ","Brain-Griffin","Metal-Jesus"];
 var badImg =  ["Booty-Warrior","Burn-Kitty","Not-Bad","Not-Okay-Rage-Face"];
-
-
-
-
 
 
 
